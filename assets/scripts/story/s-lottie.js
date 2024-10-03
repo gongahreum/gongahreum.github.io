5053ceaaf7862d18536a17fe464d9decb0296a0f
gt <gt@bysuco.com>
2024-10-03 20:01:35 +0900

    경로 변경

--- assets/scripts/story/s-lottie.js
+++ assets/scripts/story/s-lottie.js
@@ -1,30 +1,30 @@
-window.storyLottie2 = document.querySelector('#story-lottie2 .content-wrap');
-window.storyLottie2M = document.querySelector('#story-lottie2 .content-wrap');
-window.storyLottie4 = document.querySelector('#story-lottie4 .lc4');
+window.storyLottie2 = document.querySelector("#story-lottie2 .content-wrap");
+window.storyLottie2M = document.querySelector("#story-lottie2 .content-wrap");
+window.storyLottie4 = document.querySelector("#story-lottie4 .lc4");
 window.storyLottieAni2 = lottie.loadAnimation({
   container: storyLottie2,
-  renderer: 'svg',
+  renderer: "svg",
   loop: false,
   autoplay: false,
   rendererSettings: {
-    className: 'lottie-svg'
+    className: "lottie-svg",
   },
-  path: './assets/json/story/intro2.json'
+  path: "/assets/json/story/intro2.json",
 });
 window.storyLottieAni2M = lottie.loadAnimation({
   container: storyLottie2M,
-  renderer: 'svg',
+  renderer: "svg",
   loop: false,
   autoplay: false,
   rendererSettings: {
-    className: 'lottie-svg-mobile'
+    className: "lottie-svg-mobile",
   },
-  path: './assets/json/story/intro2_m.json'
+  path: "/assets/json/story/intro2_m.json",
 });
 window.storyLottieAni4 = lottie.loadAnimation({
   container: storyLottie4,
-  renderer: 'canvas',
+  renderer: "canvas",
   loop: false,
   autoplay: false,
-  path: './assets/json/story/wave.json'
+  path: "/assets/json/story/wave.json",
 });
