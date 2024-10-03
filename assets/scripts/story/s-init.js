cd6740a84bfc2df0a3a1d3a0789287982048f685
gt <gt@bysuco.com>
2024-10-03 20:10:53 +0900

    Update s-init.js

--- assets/scripts/story/s-init.js
+++ assets/scripts/story/s-init.js
@@ -1,36 +1,36 @@
-window.storyLottie1 = document.querySelector('#story-lottie1 .content-wrap');
-window.storyLottie1M = document.querySelector('#story-lottie1 .content-wrap');
+window.storyLottie1 = document.querySelector("#story-lottie1 .content-wrap");
+window.storyLottie1M = document.querySelector("#story-lottie1 .content-wrap");
 var loadedPercent;
 window.storyLottieAni1 = lottie.loadAnimation({
   container: storyLottie1,
-  renderer: 'svg',
+  renderer: "svg",
   loop: false,
   autoplay: false,
   rendererSettings: {
-    className: 'lottie-svg',
-    progressiveLoad: false
+    className: "lottie-svg",
+    progressiveLoad: false,
   },
-  path: storyLottie1.getAttribute('data-lottie')
+  path: storyLottie1.getAttribute("data-lottie"),
 });
 //mobile
 window.storyLottieAni1M = lottie.loadAnimation({
   container: storyLottie1M,
-  renderer: 'svg',
+  renderer: "svg",
   loop: false,
   autoplay: false,
   rendererSettings: {
-    className: 'lottie-svg-mobile',
-    progressiveLoad: false
+    className: "lottie-svg-mobile",
+    progressiveLoad: false,
   },
-  path: storyLottie1.getAttribute('data-lottie-m')
+  path: storyLottie1.getAttribute("data-lottie-m"),
 });
 window.getScrollbarWidth = function () {
-  var outer = document.createElement('div');
-  outer.style.visibility = 'hidden';
-  outer.style.overflow = 'scroll';
-  outer.style.msOverflowStyle = 'scrollbar';
+  var outer = document.createElement("div");
+  outer.style.visibility = "hidden";
+  outer.style.overflow = "scroll";
+  outer.style.msOverflowStyle = "scrollbar";
   document.body.appendChild(outer);
-  var inner = document.createElement('div');
+  var inner = document.createElement("div");
   outer.appendChild(inner);
   var scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
   outer.parentNode.removeChild(outer);
@@ -40,51 +40,71 @@ var horizontalGuideSet;
 var horizontalGuide = function horizontalGuide() {
   // feed 1
   var portraitRatio = window.innerWidth / window.innerHeight;
-  if (document.documentElement.classList.contains('mobile') && window.innerWidth <= 1000) {
-    if (!window.matchMedia('(orientation: portrait)').matches && portraitRatio > 1.7) {
-      if (!document.querySelector('.section-voice .text-input').classList.contains('focus')) {
-        document.querySelector('.horizontal-message').classList.add('show');
-        document.documentElement.classList.add('scroll-hide');
-        document.documentElement.classList.add('bkBg');
+  if (
+    document.documentElement.classList.contains("mobile") &&
+    window.innerWidth <= 1000
+  ) {
+    if (
+      !window.matchMedia("(orientation: portrait)").matches &&
+      portraitRatio > 1.7
+    ) {
+      if (
+        !document
+          .querySelector(".section-voice .text-input")
+          .classList.contains("focus")
+      ) {
+        document.querySelector(".horizontal-message").classList.add("show");
+        document.documentElement.classList.add("scroll-hide");
+        document.documentElement.classList.add("bkBg");
       }
     } else {
-      document.querySelector('.horizontal-message').classList.remove('show');
-      document.documentElement.classList.remove('scroll-hide');
-      document.documentElement.classList.remove('bkBg');
+      document.querySelector(".horizontal-message").classList.remove("show");
+      document.documentElement.classList.remove("scroll-hide");
+      document.documentElement.classList.remove("bkBg");
     }
   }
   clearTimeout(horizontalGuideSet);
   horizontalGuideSet = setTimeout(function () {
-    document.querySelector('.sticky-wrap.sticky1 .sticky-con').style.height = window.innerHeight + 'px';
+    document.querySelector(".sticky-wrap.sticky1 .sticky-con").style.height =
+      window.innerHeight + "px";
   }, 150);
-  var scrollParent = !document.documentElement.classList.contains('mobile') ? window : document.querySelector('#story-wrap');
-  var currentScrollOffset = !document.documentElement.classList.contains('mobile') ? window.pageYOffset : document.querySelector('#story-wrap').scrollTop;
+  var scrollParent = !document.documentElement.classList.contains("mobile")
+    ? window
+    : document.querySelector("#story-wrap");
+  var currentScrollOffset = !document.documentElement.classList.contains(
+    "mobile"
+  )
+    ? window.pageYOffset
+    : document.querySelector("#story-wrap").scrollTop;
   scrollParent.scrollTo({
     top: currentScrollOffset + 10,
-    behavior: 'smooth'
+    behavior: "smooth",
   });
 };
 
 // init
 window.scrollTo(0, 0);
-document.getElementById('story-scroll-container').style.paddingRight = getScrollbarWidth() + 'px';
-document.querySelector('.does-page-btn').style.height = window.innerHeight + 'px';
-document.querySelector('.does-page-btn').style.right = getScrollbarWidth() + 'px';
+document.getElementById("story-scroll-container").style.paddingRight =
+  getScrollbarWidth() + "px";
+document.querySelector(".does-page-btn").style.height =
+  window.innerHeight + "px";
+document.querySelector(".does-page-btn").style.right =
+  getScrollbarWidth() + "px";
 var startLottie = window.innerWidth > 1000 ? storyLottieAni1 : storyLottieAni1M;
 startLottie.addEventListener("DOMLoaded", function () {
   var ff = 0;
   var loadAnimationStart = true;
   var loadFrame = startLottie.totalFrames;
-  document.getElementById('story-wrap').style.opacity = 1;
+  document.getElementById("story-wrap").style.opacity = 1;
   horizontalGuide();
   function dataAnimation() {
     startLottie.goToAndStop(ff, true);
     // if (ff <= loadFrame) {
     if (ff <= 90) {
       if (window.innerWidth > 767) {
-        ff += .3;
+        ff += 0.3;
       } else {
-        ff += .5;
+        ff += 0.5;
       }
       requestAnimationFrame(dataAnimation);
     } else {
@@ -100,22 +120,24 @@ startLottie.addEventListener("DOMLoaded", function () {
     }
   }
   function loadAnimation() {
+    console.log("a");
     startLottie.goToAndStop(ff, true);
     // if (ff <= loadFrame) {
     if (ff <= 90) {
       if (window.innerWidth > 767) {
-        ff += .3;
+        ff += 0.3;
       } else {
-        ff += .5;
+        ff += 0.5;
       }
       requestAnimationFrame(loadAnimation);
     } else {
       window.cancelAnimationFrame(loadAnimation);
+      console.log("b");
       window.STORY.load();
     }
   }
   var xmlhttp = new XMLHttpRequest(),
-    method = 'GET',
+    method = "GET",
     url = window.location.href;
   xmlhttp.open(method, url, true);
   xmlhttp.onprogress = function (event) {
@@ -126,4 +148,4 @@ startLottie.addEventListener("DOMLoaded", function () {
   };
   xmlhttp.send();
 });
-window.addEventListener('resize', horizontalGuide);
+window.addEventListener("resize", horizontalGuide);
